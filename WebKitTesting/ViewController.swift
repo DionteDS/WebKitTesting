b0409e2da8c405bd79c7ddcde330b6328cc23189
Dionte <Dionte_sb@yahoo.com>
2019-09-25 23:50:45 -0700

    Allow the user to enter a website using the textField

--- WebKitTesting/ViewController.swift
+++ WebKitTesting/ViewController.swift
@@ -9,26 +9,54 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var webView: WKWebView!
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var urlTextField: UITextField!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        urlTextField.delegate = self
         
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        // Create the url
+        
         let urlString:String = "https://www.Google.com"
+        let url:URL = URL(string: urlString)!
+        
+        // Create the request to grab the url contents
+        
+        let urlRequest:URLRequest = URLRequest(url: url)
+        
+        // Load the request
+        
+        webView.load(urlRequest)
+        
+        // Set the textField to the url string
+        urlTextField.text = urlString
+    }
+    
+    //MARK: - TextField Methods
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        let urlString:String = urlTextField.text!
         let url:URL = URL(string: urlString)!
         let urlRequest:URLRequest = URLRequest(url: url)
         
         webView.load(urlRequest)
+        
+        textField.resignFirstResponder()
+        
+        return true
     }
 
 
