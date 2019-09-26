05b6719a7f3e5d8a901ea387640b5e3ec0002d6e
Dionte <Dionte_sb@yahoo.com>
2019-09-25 23:40:05 -0700

    Set up the webkit view and buttons

--- WebKitTesting/ViewController.swift
+++ WebKitTesting/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let urlString:String = "https://www.Google.com"
+        let url:URL = URL(string: urlString)!
+        let urlRequest:URLRequest = URLRequest(url: url)
+        
+        webView.load(urlRequest)
     }
 
 
